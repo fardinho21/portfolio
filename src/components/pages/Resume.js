2e6cac4ba8132a03bffc772492045e28620b0576
fardinho21 <fardinhojat8@gmail.com>
2025-02-14 06:09:46 -0500

    updated resume, removed borders

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,25 +2,25 @@ import React from "react";
 import "./Resume.css";
 
 const Resume = () => {
-    return (    
+    return (
         <div className="Resume-Wrapper">
             <main>
                 <header>
                     <h1>Hojat Fardin</h1>
-                    <p>(734)- 582 - 3748 | 6501 Yale St, Westland MI, 48185 | fardinhojat8@gmail.com </p>
+                    <p> Detroit Metropolitan Area </p>
                 </header>
 
                 <article className="resume">
                     <h2 className="goal">Goal</h2>
                     <section>
-                        I want to land an Entry-Level Web Developer position.
+                        I strive to make impactful contributions for the teams I work for.
                     </section>
                 </article>
                 <h2>Education</h2>
                 <article className="resume">
 
                     <section className="dates" >
-                        <div style={{gridTemplateRows: "50% 50%"}}>
+                        <div style={{ gridTemplateRows: "50% 50%" }}>
                             <p>Aug. 2017 - May 2020</p>
                             <p>Aug. 2015 - Apr. 2017</p>
                         </div>
@@ -50,26 +50,15 @@ const Resume = () => {
                 <article className="resume">
 
                     <section className="dates">
-                        <div style={{gridTemplateRows: "40% 60%"}}>
-                            <p>Nov. 2021 - Present</p>
-                            <p>Jun. 2019 - Nov. 2019</p>
+                        <div style={{ gridTemplateRows: "40% 60%" }}>
+                            <p>Aug. 2022 - Present</p>
+                            <p>Nov. 2021 - Aug. 2022</p>
                         </div>
                     </section>
                     <section className="description">
                         <ul>
-                            <li >
-                                Iconma LLC - Software Quality Operations Associate, supporting Waymo LLC
-
-                                <p>
-                                    Utilized Waymo's proprietary software tools and workflows to configure simulation frameworks for their self-driving car platform.
-                                </p>
-                                <p>
-                                    Consistently met production metrics
-                                </p>
-                            </li>
-
                             <li>
-                                MSU - Biosystems and Agricultural Engineering, Undergraduate Research Intern.
+                                DCS Corporation - Software Engineer | Scrum Master | Test Engineer
                                 <p>
                                     Worked with Dr. Renfu Lu and his research team in preparing an apple sorter machine for an in-field demonstration
                                 </p>
@@ -83,6 +72,18 @@ const Resume = () => {
                                     Reorganized the lab space
                                 </p>
                             </li>
+                            <li >
+                                Iconma LLC - Software Quality Operations Associate, supporting Waymo LLC
+
+                                <p>
+                                    Utilized Waymo's proprietary software tools and workflows to configure simulation frameworks for their self-driving car platform.
+                                </p>
+                                <p>
+                                    Consistently met production metrics
+                                </p>
+                            </li>
+
+
                         </ul>
                     </section>
 
@@ -95,59 +96,14 @@ const Resume = () => {
                         {/* This was intentionally left blank */}
                     </section>
                     <section className="description">
-                        <ul style={{listStylePosition: "outside"}}>
-                            <p>Software Development, Responsive UI Design, Design Patterns, and Debugging</p>
+                        <ul style={{ listStylePosition: "outside" }}>
+                            <p>Software Engineering and Development: Experience with Waterfall and Agile SDLC, from requirements gathering, planning, development and delivery</p>
+                            <p>Application Secuirty: Implementing TLS (Man-in-the-Middle countermeasure), input-validation and input-sanitization (XSS attack countermeasure), and session management via tokens</p>
+                            <p>Full-Stack Development: MongoDB, Express.js, React.js, and Node.js (MERN stack)</p>
+                            <p>Programming Languages: Python3.10 (7 years), C++11 (4 years), and ES6 Javascript (4 years)</p>
+                            <p>Web Scraping: Python Scrapy and Node.js Playwright</p>
                             <p>Communication and Document Preparation</p>
-                            <p>Front-end: ReactJS, Django, Angular 8</p>
-                            <p>Back-end: NodeJS, ExpressJS</p>
-                            <p>Python, C++, and Javscript</p>
-                            <p>Web Scraping and Research</p>
                             <p>General knowledge of Computer Networks, System Administration, and Application Architecture</p>
-                        </ul>
-                    </section>
-
-                </article>
-
-                <h2>Extracurriculars</h2>
-                <article className="resume" style={{borderBottom: "5px solid black"}}>
-
-                    <section className="dates">
-                        <div style={{gridTemplateRows: "67% 33%"}}>
-                            <p>Spring 2019 - Fall 2019</p>
-                            <p>Jun. 2019 - Nov. 2019</p>
-                        </div>
-                    </section>
-                    <section className="description">
-                        <ul>
-                            <li >
-                                MSU - Strength Augmenting Robotic Exoskeleton - Software Sub-Team Leader
-
-                                <p>
-                                    Fulfilled the Software Sub-Team Leader Position
-                                </p>
-                                <p>
-                                    Established software development workflows
-                                </p>
-                                <p>
-                                    Applied the Waterfall Software Development Life-cycle to our projects
-                                </p>
-                                <p>
-                                    Instructed team members on programming microcontrollers in C
-                                </p>
-                                <p>
-                                    Developed a familiarity with embedded systems
-                                </p>
-                            </li>
-
-                            <li>
-                                Spartan Blockchain Solutions
-                                <p>
-                                    Researched blockchbain development frameworks
-                                </p>
-                                <p>
-                                    Developed my knowledge base of blockchain and its industry applications
-                                </p>
-                            </li>
                         </ul>
                     </section>
 
