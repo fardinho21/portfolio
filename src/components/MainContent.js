121c595dffa265c4e92a24e6c72b1ae3f3bdd48b
hojat <fardinhojat8@gmail.com>
2025-02-22 13:14:21 -0500

    remove console.log

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,20 +3,14 @@ import {
     BrowserRouter as Router,
     Routes,
     Route,
-    useLocation
 } from "react-router-dom";
 import "./MainContent.css";
 import AboutMe from "./pages/AboutMe";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Resume from "./pages/Resume";
-import Freelance from "./pages/Freelance";
 
 const MainContent = props => {
-    const location = useLocation();
-    useEffect(() => {
-        console.log(location.pathname)
-    })
 
     return (
         <div className="MainContent">
